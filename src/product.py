66b9a229a42d43d99adba07b712c52d337257846
WingBasil <i@vyarygin.ru>
2024-08-13 23:42:11 +0700

    add env, new funk

--- src/product.py
+++ src/product.py
@@ -17,7 +17,7 @@ class Product:
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other):
-        if type(other) is Product:
+        if type(self) == type(other):
             return self.quantity * self.price + other.quantity * other.price
         raise TypeError
 
